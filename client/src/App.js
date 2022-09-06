62b613f313a5fefa033cc383ddd62f045fa71098
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-06 15:31:17 -0400

    imported style components and added a temporary button to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Landing from './pages/Landing';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: red;
+  color: white;
+  font-size: 1rem;
+`
 
 const App = () => {
   return (
     <div>
+      <Button>Click Me</Button>
       <h1>jobify</h1>
       <Landing />
     </div>

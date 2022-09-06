26a632c6e268abdf69cf741842d1420c5b9bbba3
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-06 11:21:37 -0400

    added images to landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,10 +3,27 @@ import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 
 const Landing = () => {
+
+
+
   return (
-    <>
-        <div>Landing Page</div>
-    </>
+    <main>
+        <nav>
+            <img src={logo} alt='jobify' className='logo'/>
+        </nav>
+        <div className='container page'>
+            <div className='info'>
+                <h1>
+                    job <span>track</span> app
+                </h1>
+                <p>
+                    Some dummy text goes here. More stuff for the dummies, because dummies rule!
+                </p>
+                <button className="btn btn-hero">Login/Register</button>
+            </div>
+            <img src={main} alt="job hunt" className='img main-img'/>
+        </div>
+    </main>
   )
 }
 

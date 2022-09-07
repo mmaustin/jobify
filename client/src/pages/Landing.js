0f606374be17479d17af5ed9e626c6f8eecd3cad
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-07 09:37:36 -0400

    added link to register/login button on the landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,6 +2,7 @@ import React from 'react';
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
 import { Logo } from '../components';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
@@ -17,7 +18,7 @@ const Landing = () => {
                 <p>
                     Some dummy text goes here. More stuff for the dummies, because dummies rule!
                 </p>
-                <button className="btn btn-hero">Login/Register</button>
+                <Link to='/register' className="btn btn-hero">Login/Register</Link>
             </div>
             <img src={main} alt="job hunt" className='img main-img'/>
         </div>

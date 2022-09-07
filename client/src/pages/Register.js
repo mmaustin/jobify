b4c0ab031acbff0522916503177d82129a46231c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-07 16:00:43 -0400

    added form row component and refined register component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { Logo } from '../components';
+import {FormRow} from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 
 const initialState = {
@@ -28,18 +29,26 @@ const Register = () => {
                 <Logo/>
                 <h3>Login</h3>
                 {/* name input */}
-                <div className='form-row'>
-                    <label htmlFor='name' className='form-label'>
-                        name
-                    </label>
-                    <input
-                    type="text"
+                <FormRow
+                    type='text'
                     name='name'
                     value={values.name}
-                    onChange={handleChange}
-                    className='form-input'
-                    />
-                </div>
+                    handleChange={handleChange}
+                />
+                {/* email input */}
+                <FormRow
+                    type='email'
+                    name='email'
+                    value={values.email}
+                    handleChange={handleChange}
+                />
+                {/* password input */}
+                <FormRow
+                    type='password'
+                    name='password'
+                    value={values.password}
+                    handleChange={handleChange}
+                />
                 <button type='submit' className='btn btn-block'>
                     submit
                 </button>

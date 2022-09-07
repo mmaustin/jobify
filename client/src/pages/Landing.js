99eb99ae3a733249bbced9dfd3d56ceaa6090d2f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-07 08:00:07 -0400

    added styled component styles to landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -34,6 +34,33 @@ const Wrapper = styled.main`
         display: flex;
         align-items: center;
     }
+    .page {
+        min-height: calc(100vh - var(--nav-height));
+        display: grid;
+        align-items: center;
+        margin-top: -3rem;
+    }
+    h1 {
+        font-weight: 700;
+        span{
+            color: var(--primary-500);
+        }
+    }
+    p {
+        color: var(--grey-600);
+    }
+    .main-img {
+        display: none;
+    }
+    @media (min-width: 992px) {
+        .page {
+            grid-template-columns: 1fr 1fr;
+            column-gap: 3rem;
+        }
+        .main-img {
+            display: block;
+        }
+    }
 `
 
 export default Landing;

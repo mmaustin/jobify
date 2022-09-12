18c948e83411fce6c21c0dc11f7efa41bbe6ffe8
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-12 12:42:54 -0400

    imported auth routes to server file and applied app use express json

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
+import authRouter from './routes/authRoutes.js';
 
 
 import connectDB from './db/connect.js';
@@ -9,14 +10,17 @@ import connectDB from './db/connect.js';
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
-const port = process.env.PORT || 5001;
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome!');
 })
 
+app.use('api/v1/auth', authRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5001;
 
 
 const start = async () => {

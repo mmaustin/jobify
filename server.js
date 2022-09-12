84817bb936cfdb788cb98ec55a757bac3213cd5e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-12 09:28:54 -0400

    set up error handler middleware

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 const app = express();
 
 const port = process.env.PORT || 5001;
-
-notFoundMiddleware;
 
 app.get('/', (req, res) => {
     res.send('Welcome!');
 })
 
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 app.listen(port, ()=> {
     console.log(`Server is listening on port ${port}`);

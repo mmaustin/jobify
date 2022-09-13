714b2f405f5019a2401bde6cd8bb848b8796f772
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-13 13:38:39 -0400

    installed http status codes and applied codes in auth controller and error handler module

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,12 @@
+import {StatusCodes} from 'http-status-codes';
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-    res.status(500).json({msg: err.message});
+
+    const defaultError = {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: 'Something went wrong, playa!'
+    };
+    res.status(defaultError.statusCode).json({msg: err});
 }
 
 export default errorHandlerMiddleware;

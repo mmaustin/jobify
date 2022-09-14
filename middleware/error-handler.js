8c4fa44cd3ffc4e994b15426dea71aefd715aded
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-14 09:31:27 -0400

    added controller error message to error handler middleware, if present

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,10 @@
 import {StatusCodes} from 'http-status-codes';
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-
+    console.log(err.message);
     const defaultError = {
         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        msg: 'Something went wrong, playa!'
+        msg: err.message || 'Something went wrong, playa!'
     };
     if(err.name === 'ValidationError'){
         defaultError.statusCode = StatusCodes.BAD_REQUEST

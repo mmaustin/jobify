5e52bb50295087d567283043877c23fda858dc1f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-14 09:13:13 -0400

    handled error for non-unique value

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -13,6 +13,10 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         defaultError.msg = Object.values(err.errors).map(item => item.message)
         .join(' and ');
     }
+    if(err.code && err.code === 11000){
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        defaultError.msg = `${Object.keys(err.keyValue)} has to be unique.`
+    }
     //res.status(defaultError.statusCode).json({msg: err});
     res.status(defaultError.statusCode).json({msg: defaultError.msg});
 }

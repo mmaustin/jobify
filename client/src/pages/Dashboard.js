b616f37d29d96c25957da3ed573e823692ba1a87
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-16 11:47:08 -0400

    configured proxy on client package json with local server address

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
 
 const Dashboard = () => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api/v1');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  useEffect(()=>{
+    fetchData();
+  }, []);
+
   return (
     <h1>Dashboard Here</h1>
   )

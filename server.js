8bdb54bc8baae825f2b177d337df0687b19b51cf
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-19 08:50:05 -0400

    created and exported unauthenticated error file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 import 'express-async-errors';
+import morgan from 'morgan';
 
 import connectDB from './db/connect.js';
 
@@ -13,11 +14,15 @@ import jobsRouter from './routes/jobsRoutes.js';
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
+if(process.env.NODE_ENV !== 'production'){
+    app.use(morgan('dev'));
+}
 app.use(express.json());
 
-/*app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.json({msg: 'Welcome!'});
-})*/
+})
+
 app.get('/api/v1', (req, res) => {
     res.json({msg: 'API'});
 })

fafca3ef0746163e768494d625d0ed215c0bf245
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-19 08:29:47 -0400

    installed morgan library

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -19,7 +19,7 @@ const reducer = (state, action) => {
         return {...state, isLoading: true};
     }
     if(action.type === REGISTER_USER_SUCCESS){
-        return {...state, isLoading: false, 
+        return {...state, isLoading: false,
             token: action.payload.token,
             user: action.payload.user,
             userLocation: action.payload.location,

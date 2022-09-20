66fe82999e8e866c64645ec1241604fbe4b90d6e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-20 08:49:31 -0400

    removed dashboard component from app js component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import {Landing} from './pages'
-import { Error } from './pages';
-import {Dashboard} from './pages';
-import {Register} from './pages';
+import {Register, Landing, Error} from './pages'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-
 
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard/>} />
+        <Route path="/" element={<div>Dashboard</div>} />
         <Route path="/register" element={<Register/>} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error/>}/>

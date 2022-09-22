908c51aab1f315237563bf2173a2712fde8f7c03
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-22 09:32:18 -0400

    added imports and inital return values for small sidebar component

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,10 +1,26 @@
-import React from 'react';
+//import React from 'react';
+import { FaTimes } from 'react-icons/fa';
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import links from '../utils/links';
+import {NavLink} from 'react-router-dom';
+import Logo from './Logo';
+import {useAppContext} from '../context/appContext';
+
 
 const SmallSidebar = () => {
   return (
     <Wrapper>
-        <h4>Small Sidebar</h4>
+        <div className='sidebar-container show-sidebar'>
+          <div className='content'>
+            <button type="button" className='close-btn' onClick={()=> console.log('button clicked')}>
+              <FaTimes/>
+            </button>
+            <header>
+              <Logo/>
+            </header>
+            <div className='nav-links'>nav links</div>
+          </div>
+        </div>
     </Wrapper>
   )
 }

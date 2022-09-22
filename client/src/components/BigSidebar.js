e204c8d35496b1e6948ebf55cb5297cb52fb7078
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-22 12:14:45 -0400

    completed big sidebar setup

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/BigSidebar';
+import { useAppContext } from '../context/appContext';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const BigSidebar = () => {
+  const {showSidebar} = useAppContext();
   return (
     <Wrapper>
-        <h4>Big Sidebar</h4>
+        <div className={showSidebar? 'sidebar-container' : 'sidebar-container show-sidebar'}>
+          <div className='content'>
+            <header>
+              <Logo/>
+            </header>
+            <NavLinks />
+          </div>
+        </div>
     </Wrapper>
   )
 }

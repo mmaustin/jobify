8eb4719ca0cdcd718c77c82533fda4692371cec7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-22 09:53:34 -0400

    set up functionality to toggle sidebar display

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -8,11 +8,12 @@ import {useAppContext} from '../context/appContext';
 
 
 const SmallSidebar = () => {
+  const {showSidebar, toggleSidebar} = useAppContext();
   return (
     <Wrapper>
-        <div className='sidebar-container show-sidebar'>
+        <div className={showSidebar ? 'sidebar-container show-sidebar': 'sidebar-container'}>
           <div className='content'>
-            <button type="button" className='close-btn' onClick={()=> console.log('button clicked')}>
+            <button type="button" className='close-btn' onClick={toggleSidebar}>
               <FaTimes/>
             </button>
             <header>

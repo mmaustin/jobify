20a5f9a88251a657c849e1034e72d57dca57e282
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-24 12:42:57 -0400

    added verify jwt functionality to auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,19 @@
+import { UnauthenticatedError } from "../errors/index.js";
+import jwt from 'jsonwebtoken';
+
 const auth = async (req, res, next) => {
-    console.log('authenticate user');
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith('Bearer')) {
+        throw new UnauthenticatedError('Authentication Invalid');
+    }
+
+    const token = authHeader.split(' ')[1];
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+        throw new UnauthenticatedError('Authentication Invalid');
+    }
+
     next();
 }
 

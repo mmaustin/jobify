335e7d2e595ab7d2d80287820714571a52c9968b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-25 12:11:55 -0400

    imported user and set up local state values in profile file

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,8 +1,21 @@
-import React from 'react'
-import { useAppContext } from '../../context/appContext'
+import React, {useState} from 'react';
+import { useAppContext } from '../../context/appContext';
+import { FormRow, Alert } from '../../components';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
 
 const Profile = () => {
-  const {user} = useAppContext();
+  const {user, showAlert, displayAlert, updateUser, isLoading} = 
+  useAppContext();
+
+  const [name, setName] = useState(user?.name);
+  const [eamil, setEmail] = useState(user?.email);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [location, setLocation] = useState(user?.location);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('update user');
+  }
 
   return (
     <h1>Profile Page</h1>

21b6d78d70ce1e2170293c90cc0b673a904dca0b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-25 10:35:33 -0400

    set up initial functionality for update user controller

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,6 +10,8 @@ const auth = async (req, res, next) => {
     const token = authHeader.split(' ')[1];
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = {userId: payload.userId};
+        //console.log(req.user.userId);
     } catch (error) {
         throw new UnauthenticatedError('Authentication Invalid');
     }

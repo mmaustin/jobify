86731d641d9eb2e89c62f4bc618365b6096b2ec7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-26 10:03:31 -0400

    used axios defaults to set the authorization header bearer token

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -34,6 +34,8 @@ const AppContext = React.createContext();
 const AppProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
+    axios.defaults.headers.common['Authorization'] = `Bearer ${state.token}`;
+
     const displayAlert = () => {
         dispatch({type: DISPLAY_ALERT});
         clearAlert();
@@ -103,13 +105,7 @@ const AppProvider = ({children}) => {
 
     const updateUser = async (currentUser) => {
         try {
-            const {data} = await axios.patch('/api/v1/auth/updateUser',
-            currentUser,
-            {
-                headers: {
-                    Authorization: `Bearer ${state.token}`,
-                }
-            })
+            const {data} = await axios.patch('/api/v1/auth/updateUser', currentUser)
             console.log(data);
         } catch (error) {
             console.log(error.response);

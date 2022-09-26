3183ef9478748994a22071a4b1c131a8c9fa3087
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-26 15:56:00 -0400

    created job model

---
+++ models/Job.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import User from "./User";
+
+const JobSchema = new mongoose.Schema({
+        company: {
+            type: String,
+            required: [true, 'Please provide company'],
+            maxlength: 50,
+        },
+        position: {
+            type: String,
+            required: [true, 'Please provide position'],
+            maxlength: 100,
+        },
+        status: {
+            type: String,
+            enum: ['inteview', 'declined', 'pending'],
+            default: pending,
+        },
+        jobType: {
+            type: String,
+            enum: ['full-time', 'part-time', 'remote', 'internship'],
+            default: 'full-time',
+        },
+        jobLocation: {
+            type: String,
+            default: 'my city',
+            required: true,
+        },
+        createdBy: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+            required: [true, 'Please provide user']
+        }
+    },
+    {timestamps: true}
+);
+
+export default mongoose.model('Job', JobSchema);

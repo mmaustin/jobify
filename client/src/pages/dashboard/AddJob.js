555f817d040ee167a2b7b1081f4e7212b40e7ee4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-27 11:11:26 -0400

    created a handle job input function to log changes to input values

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -8,11 +8,25 @@ const AddJob = () => {
   const {isEditing, showAlert, displayAlert, position, company, jobLocation,
   jobType, jobTypeOptions, status, statusOptions} = useAppContext();
 
+  const handleJobInput = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(`${name}: ${value}`);
+  }
+
   return (
     <Wrapper>
       <form className='form'>
         <h3>{isEditing ? 'edit job' : 'add job'}</h3>
-        {showAlert}
+        {showAlert && <Alert/>}
+        <div className='form-center'>
+          <FormRow
+            type="text"
+            name="position"
+            value={position}
+
+          />
+        </div>
       </form>
     </Wrapper>
   )

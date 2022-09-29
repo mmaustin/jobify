87d3ab56d8c285b56f3fff86add266fc759186e7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-29 13:04:41 -0400

    added initial functionality for retrieving all jobs from the server

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -13,8 +13,9 @@ const createJob = async (req, res) => {
     res.status(StatusCodes.CREATED).json({job});
 }
 
-const getAllJobs = (req, res) => {
-    res.send('Get all jobs');
+const getAllJobs = async (req, res) => {
+    const jobs = await Job.find({createdBy: req.user.userId});
+    res.status(StatusCodes.OK).json({jobs, totalJobs:jobs.length, numOfPages:1})
 }
 
 const updateJob = (req, res) => {

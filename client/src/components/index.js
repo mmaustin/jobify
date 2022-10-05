346841ef15cd7c4d252d87b70350525b623fabbe
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-05 12:48:34 -0400

    created three stats page components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -7,6 +7,9 @@ import SmallSidebar from "./SmallSidebar";
 import FormRowSelect from "./FormRowSelect";
 import SearchContainer from "./SearchContainer";
 import JobsContainer from "./JobsContainer";
+import StatsContainer from "./StatsContainer";
+import ChartsContainer from "./ChartsContainer";
+
 
 export {Logo,
     FormRow,
@@ -16,5 +19,7 @@ export {Logo,
     SmallSidebar,
     FormRowSelect,
     SearchContainer,
-    JobsContainer
+    JobsContainer,
+    StatsContainer,
+    ChartsContainer
 };

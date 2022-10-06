44df9ab9f6841fa09df45024eb9cebcb98d4dd1a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-06 11:25:01 -0400

    set up the ui for the area chart component

--- client/src/components/AreaChart.js
+++ client/src/components/AreaChart.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import {
+    ResponsiveContainer,
+    AreaChart,
+    Area,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip
+} from 'recharts'
 
 
-const AreaChart = () => {
+const AreaChartComponent = ({data}) => {
   return (
-    <h2>Area Chart</h2>
+    <ResponsiveContainer width='100%' height={300}>
+        <AreaChart data={data} margin={{top:50}}>
+          <CartesianGrid strokeDasharray='3 3 '/>
+          <XAxis dataKey='date'/>
+          <YAxis allowDecimals={false}/>
+          <Tooltip/>
+          <Area dataKey='count' fill='#2db1bc' barSize={75}/>
+        </AreaChart>
+    </ResponsiveContainer>
   )
 }
 
-export default AreaChart
+export default AreaChartComponent;

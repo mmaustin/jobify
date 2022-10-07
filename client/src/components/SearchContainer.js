83b6f7c776d80e67c9169650f9bb1dd03e01bce3
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-07 13:47:01 -0400

    set up intial functionality for the search container component

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useAppContext } from '../context/appContext';
-import {FormRow, FormRowSelect} from '../components';
+import {FormRow, FormRowSelect} from '.';
 import Wrapper from '../assets/wrappers/SearchContainer';
 
 const SearchContainer = () => {
@@ -16,9 +16,26 @@ const SearchContainer = () => {
     handleChange,
     clearFilters
   } = useAppContext()
-  
+
+  const handleSearch = (e) => {
+    if(isLoading) return
+    handleChange({name: e.target.name, value: e.target.value})
+  }
+
   return (
-    <h1>Search Component</h1>
+    <Wrapper>
+      <form className='form'>
+        <h4>search form</h4>
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='search'
+            value={search}
+            handleChange={handleSearch}
+          />
+        </div>
+      </form>
+    </Wrapper>
   )
 }
 

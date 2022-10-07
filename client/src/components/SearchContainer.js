35e1f6e56bc81448bcdf60660567048fea709372
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-07 13:12:51 -0400

    imported components and app context properties to search container component

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import { useAppContext } from '../context/appContext';
+import {FormRow, FormRowSelect} from '../components';
+import Wrapper from '../assets/wrappers/SearchContainer';
 
 const SearchContainer = () => {
+  const {
+    isLoading,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+    sortOptions,
+    statusOptions,
+    jobTypeOptions,
+    handleChange,
+    clearFilters
+  } = useAppContext()
+  
   return (
     <h1>Search Component</h1>
   )

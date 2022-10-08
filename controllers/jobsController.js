964a07db28f7382ff7a70d17cd4d3af66aeceb7f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-08 11:43:49 -0400

    completed back end pagination functionality

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -47,14 +47,17 @@ const getAllJobs = async (req, res) => {
     }
 
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.number) || 10;
+    const limit = Number(req.query.limit) || 10;
     const skip = (page - 1) * limit;
 
     result = result.skip(skip).limit(limit);
 
     const jobs = await result;
 
-    res.status(StatusCodes.OK).json({jobs, totalJobs:jobs.length, numOfPages:1})
+    const totalJobs = await Job.countDocuments(queryObject);
+    const numOfPages = Math.ceil(totalJobs/limit);
+
+    res.status(StatusCodes.OK).json({jobs, totalJobs, numOfPages})
 }
 
 const updateJob = async (req, res) => {

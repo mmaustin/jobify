a470503bf691a112d937b1e06331b790b250a811
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-10 10:45:07 -0400

    dynamically displayed page numbers in page btn containder return value

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -7,6 +7,10 @@ import Wrapper from '../assets/wrappers/PageBtnContainer';
 const PageBtnContainer = () => {
     const {numOfPages, page} = useAppContext();
 
+    const pages = Array.from({length: numOfPages}, (_,index)=>{
+        return index + 1;
+    })
+
     const nextPage = () => {
         console.log('next page')
     }
@@ -21,7 +25,18 @@ const PageBtnContainer = () => {
                 <HiChevronDoubleLeft/>
                 prev
             </button>
-            <div className='btn-container'>buttons</div>
+            <div className='btn-container'>
+                {pages.map(pageNumber => {
+                    return <button
+                        key={pageNumber}
+                        type='button'
+                        className={pageNumber === page ? 'pageBtn active' : 'pageBtn'}
+                        onClick={()=>console.log('change page')}
+                    >
+                    {pageNumber}
+                    </button>
+                })}
+            </div>
             <button className='next-btn' onClick={nextPage}>
                 next
                 <HiChevronDoubleRight/>

e6cbcd33b918d0c4fea3d725e15cf6c02b805bfa
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-11 09:12:11 -0400

    built client and installed securtiy packages

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ dotenv.config();
 import 'express-async-errors';
 import morgan from 'morgan';
 
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
 import connectDB from './db/connect.js';
 
 import authRouter from './routes/authRoutes.js';
@@ -19,18 +23,18 @@ import authenticateUser from './middleware/auth.js';
 if(process.env.NODE_ENV !== 'production'){
     app.use(morgan('dev'));
 }
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.json({msg: 'Welcome!'});
-})
-
-app.get('/api/v1', (req, res) => {
-    res.json({msg: 'API'});
-})
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+app.get('*', (req,res)=> {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.htmml'))
+})
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
